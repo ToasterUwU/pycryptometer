2cd51a0950d64b6d564fcf4e1c7903556c4656cf
ToasterUwU <marvinator11@gmail.com>
2020-09-18 19:15:18 +0200

    uploaded to PyPI

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    longd = f.read()
 
-setuptools.setup(
+setup(
     name = "pycryptometer",
-    version = "1.0.0",
+    version = "1.0.1",
     license = "MIT",
     author = "ToasterUwU",
+    author_email="toasterger@gmail.com",
     description = "API Wrapper for cryptometer.io",
-    long_description = long_description,
+    long_description = longd,
+    long_description_content_type = "text/markdown",
     url = "https://github.com/ToasterUwU/pycryptometer",
-    packages = setuptools.find_packages(),
-    keywords = ['API', 'Cryptometer'],
+    keywords = ['API-Wrapper', 'Cryptometer'],
     install_requires=[
         'requests',
     ],
     classifiers = [
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: MIT License"
+        "Operating System :: OS Independent"
     ],
     python_requires='>=3.6'
 )
